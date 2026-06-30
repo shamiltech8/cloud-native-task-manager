5acbe2a9e113fdbfad13e091578c8f59edefae04
Muhammed Shamil KT <shamil6282669@gmail.com>
2026-06-30 12:26:39 +0530

    Add static and dynamic Flask routes

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,19 @@ from app import app
 @app.route("/")
 def home():
     return "Welcome to Cloud-Native Task Manager!"
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+@app.route("/contact")
+def contact():
+    return "Contact Page"
+
+@app.route("/dashboard")
+def dashboard():
+    return "Dashboard Page"
+
+@app.route("/task/<int:id>")
+def task(id):
+    return f"Displaying Task {id}"
