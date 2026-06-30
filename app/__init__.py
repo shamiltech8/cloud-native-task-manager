cf9445b0d11597cf675c83741c790cf94837c38f
Muhammed Shamil KT <shamil6282669@gmail.com>
2026-06-30 22:44:12 +0530

    Protect dashboard using Flask sessions

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.secret_key = "my_user_key"
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, session, redirect, url_for
 from app import app
 
 @app.route("/")
@@ -15,7 +15,12 @@ def contact():
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html")
+
+    if "username" not in session:
+        return redirect(url_for("login"))
+
+    username = session["username"]
+    return render_template("dashboard.html",username=username)
 
 @app.route("/task/<int:id>")
 def task(id):
@@ -31,3 +36,23 @@ def add_task():
        return f"Task Added: {task}"
 
     return render_template("add_task.html")
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+
+        session["username"] = username
+
+        return redirect(url_for("dashboard"))
+
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+
+    session.pop("username", None)
+
+    return redirect(url_for("home"))
